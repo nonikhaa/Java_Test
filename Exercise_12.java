515c1983f8b7fa47cca924d4c27614b73be2252e
Noni <nonikhaa6@gmail.com>
2019-04-29 14:40:55 +0700

    Exercise 12

---
+++ Exercise_12.java
@@ -0,0 +1,24 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Exercise_12{
+
+    public static void main(String[] args){
+        int arr1[] = {1,2,3,4,5};
+        int arr2[] = {1,3,5,7,9};
+        int[] arr3 = new int[3];
+        int a = 0;
+
+        for (int data : arr1) {
+            for (int detail : arr2) {
+                if(data == detail)
+                {
+                    arr3[a] += data;
+                    a++;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr3)); 
+    }
+}
